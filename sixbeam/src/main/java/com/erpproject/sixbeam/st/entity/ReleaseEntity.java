b93deaa8047605cbc54538c18e5c8bb70cbd644c
KimSeonjin94 <o8o747o@gmail.com>
2024-02-01 17:48:48 +0900

    240201 whregist 데이터 조회, dto클래스 수정, 더미데이터 넣기

--- sixbeam/src/main/java/com/erpproject/sixbeam/st/entity/ReleaseEntity.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/st/entity/ReleaseEntity.java
@@ -3,11 +3,14 @@ package com.erpproject.sixbeam.st.entity;
 import com.erpproject.sixbeam.ac.entity.AccountEntity;
 import com.erpproject.sixbeam.hr.entity.EmpInfoEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
-
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Getter
 @Setter
