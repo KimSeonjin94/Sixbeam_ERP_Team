582820000bc71f4f765b75e3bdf42c81efd5054e
LIMGEES <dlawlghks04@nate.com>
2024-02-01 23:56:39 +0900

    02.01 스크립트 수정 및 html 수정

--- sixbeam/src/main/resources/static/js/demo/datatables-demo.js
+++ sixbeam/src/main/resources/static/js/demo/datatables-demo.js
@@ -1,4 +1,46 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+    $('#dataTable').DataTable({
+        "language": {
+            "emptyTable": "데이터가 없어요.",
+            "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+            "info": "현재 _START_ - _END_ / _TOTAL_건",
+            "infoEmpty": "데이터 없음",
+            "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+            "search": "검색: ",
+            "zeroRecords": "일치하는 데이터가 없어요.",
+            "loadingRecords": "로딩중...",
+            "processing":     "잠시만 기다려 주세요...",
+            "paginate": {
+                "next": "다음",
+                "previous": "이전"
+            }},
+        "buttons": ['copy', 'excel', 'pdf', 'print'],
+
+    },
+        {
+            dom: "Bfrtip",
+            buttons: [
+                {
+                    extend: "excel",                    // Extend the excel button
+                    excelStyles: {                      // Add an excelStyles definition
+                        cells: "2",                     // to row 2
+                        style: {                        // The style block
+                            font: {                     // Style the font
+                                name: "Arial",          // Font name
+                                size: "14",             // Font size
+                                color: "FFFFFF",        // Font Color
+                                b: false,               // Remove bolding from header row
+                            },
+                            fill: {                     // Style the cell fill (background)
+                                pattern: {              // Type of fill (pattern or gradient)
+                                    color: "457B9D",    // Fill color
+                                }
+                            }
+                        }
+                    },
+                },
+            ],
+        });
 });
+
--- sixbeam/src/main/resources/static/js/sb-admin-2.js
+++ sixbeam/src/main/resources/static/js/sb-admin-2.js
@@ -1,56 +1,88 @@
 (function($) {
-  "use strict"; // Start of use strict
+    "use strict"; // Start of use strict
 
-  // Toggle the side navigation
-  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
-    $("body").toggleClass("sidebar-toggled");
-    $(".sidebar").toggleClass("toggled");
-    if ($(".sidebar").hasClass("toggled")) {
-      $('.sidebar .collapse').collapse('hide');
-    };
-  });
+    // Toggle the side navigation
+    $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+        $("body").toggleClass("sidebar-toggled");
+        $(".sidebar").toggleClass("toggled");
+        if ($(".sidebar").hasClass("toggled")) {
+            $('.sidebar .collapse').collapse('hide');
+        };
+    });
 
-  // Close any open menu accordions when window is resized below 768px
-  $(window).resize(function() {
-    if ($(window).width() < 768) {
-      $('.sidebar .collapse').collapse('hide');
-    };
-    
-    // Toggle the side navigation when window is resized below 480px
-    if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
-      $("body").addClass("sidebar-toggled");
-      $(".sidebar").addClass("toggled");
-      $('.sidebar .collapse').collapse('hide');
-    };
-  });
+    // Close any open menu accordions when window is resized below 768px
+    $(window).resize(function() {
+        if ($(window).width() < 768) {
+            $('.sidebar .collapse').collapse('hide');
+        };
 
-  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
-    if ($(window).width() > 768) {
-      var e0 = e.originalEvent,
-        delta = e0.wheelDelta || -e0.detail;
-      this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-      e.preventDefault();
-    }
-  });
+        // Toggle the side navigation when window is resized below 480px
+        if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
+            $("body").addClass("sidebar-toggled");
+            $(".sidebar").addClass("toggled");
+            $('.sidebar .collapse').collapse('hide');
+        };
+    });
 
-  // Scroll to top button appear
-  $(document).on('scroll', function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
+    // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+    $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+        if ($(window).width() > 768) {
+            var e0 = e.originalEvent,
+            delta = e0.wheelDelta || -e0.detail;
+            this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+            e.preventDefault();
+        }
+    });
 
-  // Smooth scrolling using jQuery easing
-  $(document).on('click', 'a.scroll-to-top', function(e) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: ($($anchor.attr('href')).offset().top)
-    }, 1000, 'easeInOutExpo');
-    e.preventDefault();
-  });
+    // Scroll to top button appear
+    $(document).on('scroll', function() {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
+
+    // Smooth scrolling using jQuery easing
+    $(document).on('click', 'a.scroll-to-top', function(e) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top)
+        }, 1000, 'easeInOutExpo');
+        e.preventDefault();
+    });
+
+
+    $(document).ready(function () {
+        var activeTabId = localStorage.getItem('activeTabId');  // 활성 탭 ID를 Local Storage에서 읽어옴
+
+        // 초기 로딩 시 저장된 활성 탭 ID를 기준으로 해당 탭을 활성화
+        if (activeTabId) {
+            $('.tab-pane').removeClass('show').removeClass('active');
+            $('#Tap-' + activeTabId).addClass('show').addClass('active');
+        }
+
+        // 각 버튼에 대한 클릭 이벤트 처리
+        $('[data-tab-id]').click(function () {
+            // 모든 탭을 숨기고 클릭한 탭에 대한 사이드바를 보여줌
+            $('.tab-pane').removeClass('show').removeClass('active');
+            $('#Tap-' + $(this).data('tab-id')).addClass('show').addClass('active');
+
+            // 활성 탭 ID 저장
+            activeTabId = $(this).data('tab-id');
+            localStorage.setItem('activeTabId', activeTabId);  // 활성 탭 ID를 Local Storage에 저장
+        });
+
+        // 각 탭 내부의 링크에 대한 클릭 이벤트 처리
+        $('.tab-pane a').click(function () {
+            // 저장된 활성 탭을 기준으로 해당 탭을 활성화
+            if (activeTabId) {
+                $('.tab-pane').removeClass('show').removeClass('active');
+                $('#Tap-' + activeTabId).addClass('show').addClass('active');
+            }
+        });
+    });
+
 
 })(jQuery); // End of use strict
