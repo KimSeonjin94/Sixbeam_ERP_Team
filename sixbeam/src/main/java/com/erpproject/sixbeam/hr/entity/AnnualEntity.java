44b6a5a2132c83a4b0d489238f6ad9bf5b22626b
wonangs <kyh1281@kangwon.ac.kr>
2024-02-02 17:53:16 +0900

    240202 1752 내용 수정 無 점심때와 동일

--- sixbeam/src/main/java/com/erpproject/sixbeam/hr/entity/AnnualEntity.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/hr/entity/AnnualEntity.java
@@ -1,7 +1,6 @@
 package com.erpproject.sixbeam.hr.entity;
 
 import java.time.LocalDate;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,26 +13,26 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "HR_ANNNAL_TB")
+@Table(name="HR_ANNNAL_TB")
 public class AnnualEntity {
     @Id
-    @Column(name = "empinfoId", insertable = false, updatable = false)
+    @Column(name="empinfoId",insertable = false,updatable = false)
     private String empInfoId;
     @ManyToOne
     @JoinColumn(name = "empinfoId")
     private EmpInfoEntity empInfoEntity;//사원Id
     @Column(name = "annualDt")
     private LocalDate annualDt;//신청날짜
-    @Column(name = "annualCnt")
+    @Column(name ="annualCnt")
     private int annualCnt;//연차개수
-    @Column(name = "annualApply")
+    @Column(name ="annualApply")
     private boolean annualApply;//연차신청
-    @Column(name = "annualStartDt")
+    @Column(name ="annualStartDt")
     private LocalDate annualStartDt;//연차시작
-    @Column(name = "annualFinishDt")
+    @Column(name ="annualFinishDt")
     private LocalDate annualFinishDt;//연차 끝
     @ManyToOne
-    @JoinColumn(name = "reasonCd")
+    @JoinColumn(name ="reasonCd")
     private ReasonEntity reasonEntity;//사유코드
 
 }
