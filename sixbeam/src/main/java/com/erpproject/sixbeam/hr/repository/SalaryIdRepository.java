f9cb09db52e857246fab02e3d0c49cf1bc5055e0
pjh4554 <dnflgkrry28@gmail.com>
2024-02-02 17:51:33 +0900

    2024-02-02 17:50 더미데이터 생성 과 템플릿에 사원의 정보까지 표시

---
+++ sixbeam/src/main/java/com/erpproject/sixbeam/hr/repository/SalaryIdRepository.java
@@ -0,0 +1,8 @@
+package com.erpproject.sixbeam.hr.repository;
+
+
+import com.erpproject.sixbeam.hr.entity.SalaryIdEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalaryIdRepository extends JpaRepository<SalaryIdEntity,Long> {
+}
