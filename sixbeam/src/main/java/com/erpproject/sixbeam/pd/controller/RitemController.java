b92d78b5fc5f30ae1fc3461d7f65e2c435bdaea8
LeeSang90 <dlgy900@naver.com>
2024-02-02 16:08:18 +0900

    구매 관련 sql 파일 추가 및 RitemController 에러 수정

--- sixbeam/src/main/java/com/erpproject/sixbeam/pd/controller/RitemController.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/pd/controller/RitemController.java
@@ -1,5 +1,6 @@
 package com.erpproject.sixbeam.pd.controller;
 
+import com.erpproject.sixbeam.pd.repository.RitemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
