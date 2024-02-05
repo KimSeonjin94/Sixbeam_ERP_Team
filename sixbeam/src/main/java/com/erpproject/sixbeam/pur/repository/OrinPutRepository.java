e99a712aad2e1c7e2ba1d861b0864ee33c520db6
LeeSang90 <dlgy900@naver.com>
2024-02-05 17:52:29 +0900

    OrintPutForm추가

--- sixbeam/src/main/java/com/erpproject/sixbeam/pur/repository/OrinPutRepository.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/pur/repository/OrinPutRepository.java
@@ -3,6 +3,6 @@ package com.erpproject.sixbeam.pur.repository;
 import com.erpproject.sixbeam.pur.entity.OrinPutEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrinPutRepository extends JpaRepository<OrinPutEntity, String> {
 
+public interface OrinPutRepository extends JpaRepository<OrinPutEntity, String> {
 }
