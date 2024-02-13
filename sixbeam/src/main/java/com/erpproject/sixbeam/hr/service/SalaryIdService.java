c406aac30fd7a42c240aaba643e99094062f0bbf
pjh4554 <dnflgkrry28@gmail.com>
2024-02-14 01:51:45 +0900

    2024-02-14 01:51

--- sixbeam/src/main/java/com/erpproject/sixbeam/hr/service/SalaryIdService.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/hr/service/SalaryIdService.java
@@ -1,5 +1,7 @@
 package com.erpproject.sixbeam.hr.service;
 
+import com.erpproject.sixbeam.hr.entity.DepartEntity;
+import com.erpproject.sixbeam.hr.entity.PositionEntity;
 import com.erpproject.sixbeam.hr.entity.SalaryIdEntity;
 import com.erpproject.sixbeam.hr.repository.SalaryIdRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,11 +9,33 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class SalaryIdService {
     private final SalaryIdRepository salaryIdRepository;
-    public List<SalaryIdEntity>getList(){
+    public List<SalaryIdEntity> getList() {
         return this.salaryIdRepository.findAll();
     }
+    public void updateSalaryId(Long salaryIdCd, DepartEntity departCd, PositionEntity positionCd, int basicSalary){
+        SalaryIdEntity salaryIdEntity = salaryIdRepository.findById(salaryIdCd)
+                .orElseThrow(() -> new RuntimeException("Depart with ID "+ salaryIdCd + " not fount"));
+        salaryIdEntity.setSalaryIdCd(salaryIdCd);
+        salaryIdEntity.setDepartEntity(departCd);
+        salaryIdEntity.setPositionEntity(positionCd);
+        salaryIdEntity.setBasicSalary(basicSalary);
+        salaryIdRepository.save(salaryIdEntity);
+
+    }
+    public void createSalaryId(DepartEntity departCd, PositionEntity positionCd, int basicSalary){
+        SalaryIdEntity salaryIdEntity = new SalaryIdEntity();
+        salaryIdEntity.setDepartEntity(departCd);
+        salaryIdEntity.setPositionEntity(positionCd);
+        salaryIdEntity.setBasicSalary(basicSalary);
+        salaryIdRepository.save(salaryIdEntity);
+    }
+    public void deleteSalaryId(Long salaryIdCd){
+        this.salaryIdRepository.deleteById(salaryIdCd);
+    }
 }
+
