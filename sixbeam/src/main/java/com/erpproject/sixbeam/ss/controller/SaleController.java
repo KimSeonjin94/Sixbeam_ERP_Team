8de9a212edd31aaae80ffcf133d84f1b07e2ce66
LIMGEES <149366415+LIMGEES@users.noreply.github.com>
2024-02-16 17:51:35 +0900

    sale 메뉴 추가 및 수정

--- sixbeam/src/main/java/com/erpproject/sixbeam/ss/controller/SaleController.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/ss/controller/SaleController.java
@@ -1,11 +1,47 @@
 package com.erpproject.sixbeam.ss.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.erpproject.sixbeam.ss.dto.SaleDto;
+import com.erpproject.sixbeam.ss.entity.EstimateEntity;
+import com.erpproject.sixbeam.ss.entity.SaleEntity;
+import com.erpproject.sixbeam.ss.service.EstimateService;
+import com.erpproject.sixbeam.ss.service.SaleService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/ss/sale")
+@Controller
+@Slf4j
 public class SaleController {
+    @Autowired
+    private SaleService saleService;
+    @Autowired
+    private EstimateService estimateService;
+
+    public String create(@ModelAttribute SaleDto saleDto){
+        saleService.create(saleDto);
+        return "redirect:/ss/sale/list";
+    }
+    @GetMapping("/list")
+    public String list(Model model){
+        List<SaleEntity> saleEntities=saleService.getList();
+        List<EstimateEntity> estimateEntities= estimateService.getList();
+        model.addAttribute("estimateEntites",estimateEntities);
+        model.addAttribute("saleEntities",saleEntities);
+        return "contents/ss/sale_list";
+    }
+    @GetMapping("/new")
+    public String create(Model model){
+        SaleDto saleDto=new SaleDto();
+        List< EstimateEntity> estimateEntities = estimateService.getList();
+        model.addAttribute("estimateEntities",estimateEntities);
+        model.addAttribute("saleDto",saleDto);
+        return "contents/ss/sale_form";
+    }
 
 
 }
