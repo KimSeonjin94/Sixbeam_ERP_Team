32db4d66b50b799977fac196568574cdee32dd17
LeeSang90 <dlgy900@naver.com>
2024-02-21 17:46:00 +0900

    240221 1744 구매 화면에서 구매 저장 진행중,삭제는 완료

--- sixbeam/src/main/java/com/erpproject/sixbeam/pur/controller/InputController.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/pur/controller/InputController.java
@@ -1,8 +1,11 @@
 package com.erpproject.sixbeam.pur.controller;
 
 import com.erpproject.sixbeam.pur.dto.InputDto;
+import com.erpproject.sixbeam.pur.dto.OrinPutDto;
 import com.erpproject.sixbeam.pur.entity.InputEntity;
 import com.erpproject.sixbeam.pur.entity.OrinPutEntity;
+import com.erpproject.sixbeam.pur.form.InputForm;
+import com.erpproject.sixbeam.pur.form.OrinPutForm;
 import com.erpproject.sixbeam.pur.service.InputService;
 import com.erpproject.sixbeam.pur.service.OrinPutService;
 import com.erpproject.sixbeam.st.entity.WhregistEntity;
@@ -11,10 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -35,6 +36,13 @@ public class InputController {
         return "contents/pur/input_list";
     }
 
+    @GetMapping(value = "/list/detail/{id}")
+    public ResponseEntity<List<InputEntity>> detail(@PathVariable("id") String id){
+        List<InputEntity> inPutEntities = this.inputService.getIdList(id);
+        System.out.println(inPutEntities.toString());
+        return ResponseEntity.ok(inPutEntities);
+    }
+
     @GetMapping("/create")
     public String InPutCreate(Model model) {
         InputDto inputDto = new InputDto();
@@ -50,12 +58,53 @@ public class InputController {
     public ResponseEntity<?> saveInPut(@ModelAttribute InputDto inputDto) {
         try {
             this.inputService.save(inputDto);
-            return ResponseEntity.ok().body(Collections.singletonMap("redirectUrl", "/pur/input/list")); // 저장 후 목록 페이지로 리다이렉트
+            Map<String, Object> successResponse = new HashMap<>();
+            successResponse.put("status", "success");
+            successResponse.put("message", "정상적으로 저장되었습니다.");
+            successResponse.put("redirectUrl", "/pur/input/list");
+            return ResponseEntity.ok().body(successResponse); // 저장 후 목록 페이지로 리다이렉트
         } catch (Exception e) {
             Map<String, Object> errorResponse = new HashMap<>();
             errorResponse.put("status", "error");
             errorResponse.put("message", "저장에 실패 하였습니다.");
             errorResponse.put("redirectUrl", "/pur/input/create");
+            return ResponseEntity.badRequest().body(errorResponse);
+        }
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<?> updateinput(@ModelAttribute InputDto inputDto){
+        try {
+            inputService.update(inputDto);
+            Map<String, Object> successResponse = new HashMap<>();
+            successResponse.put("status", "success");
+            successResponse.put("message", "정상적으로 수정되었습니다.");
+            successResponse.put("redirectUrl", "/pur/input/list");
+            return ResponseEntity.ok().body(successResponse);
+        } catch (Exception e){
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("status", "error");
+            errorResponse.put("message", "수정에 실패 하였습니다.");
+            errorResponse.put("redirectUrl", "/pur/input/update");
+            return ResponseEntity.badRequest().body(errorResponse);
+        }
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<?> deleteinput(@RequestParam("selectedinput") List<String> selectedInputIds, RedirectAttributes redirectAttributes) {
+        try {
+            // 선택된 발주 정보를 삭제
+            inputService.delete(selectedInputIds);
+            Map<String, Object> successResponse = new HashMap<>();
+            successResponse.put("status", "success");
+            successResponse.put("message", "정상적으로 삭제되었습니다.");
+            successResponse.put("redirectUrl", "/pur/input/list");
+            return ResponseEntity.ok().body(successResponse); // 삭제 후 목록 페이지로 리다이렉트
+        } catch (IllegalStateException e) {
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("status", "error");
+            errorResponse.put("message", e.getMessage());
+            errorResponse.put("redirectUrl", "/pur/input/list");
             return ResponseEntity.badRequest().body(errorResponse);
         }
     }
--- sixbeam/src/main/java/com/erpproject/sixbeam/pur/entity/OrinPutEntityId.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/pur/entity/OrinPutEntityId.java
@@ -2,12 +2,16 @@ package com.erpproject.sixbeam.pur.entity;
 
 import com.erpproject.sixbeam.pd.entity.ItemEntity;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class OrinPutEntityId implements Serializable {
     private String orinputCd;
     private ItemEntity itemEntity;
