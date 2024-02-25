820279dbf1eb461195b03b096a224d7ba645fbcd
wonangs <kyh1281@kangwon.ac.kr>
2024-02-25 15:01:28 +0900

    0225 1501 ItemService, ItemRepository 백업용

--- sixbeam/src/main/java/com/erpproject/sixbeam/pd/service/ItemService.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/pd/service/ItemService.java
@@ -3,10 +3,12 @@ package com.erpproject.sixbeam.pd.service;
 import com.erpproject.sixbeam.pd.dto.ItemDto;
 import com.erpproject.sixbeam.pd.entity.ItemEntity;
 import com.erpproject.sixbeam.pd.repository.ItemRepository;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -78,28 +80,75 @@ public class ItemService {
     }
 
     public void saveItem(ItemDto itemDto) {
+
+        String itemNm = itemDto.getItemNm();
+        String itemStnd = itemDto.getItemStnd();
+        Long itemUp = itemDto.getItemUp();
+
+        // 품목 코드가 공백인지 확인
+        if (itemNm == null || itemNm.trim().isEmpty()) {
+            throw new IllegalArgumentException("품목명을 입력해주세요");
+
+        } else if (itemStnd == null || itemStnd.trim().isEmpty()) {
+            throw new IllegalArgumentException("규격을 입력해주세요");
+
+        } else if (itemUp == null || String.valueOf(itemUp).trim().isEmpty()) {
+            throw new IllegalArgumentException("단가를 입력해주세요");
+        }
+
+        // 새로운 아이템 코드 생성
+        String newItemCd = generateNewItemCd();
+        itemDto.setItemCd(newItemCd);
+
         ItemEntity itemEntity = itemDto.toEntity();
         itemRepository.save(itemEntity);
     }
 
+    private String generateNewItemCd() {
+        // "F"로 시작하는 새로운 품목 코드 생성
+        String prefix = "F";
+
+        // DB에서 최대 품목 코드를 가져와서 숫자 부분 추출 후 +1 증가
+        String maxCd = itemRepository.getMaxItemCdStartingWithF();
+        int sequenceNumber = maxCd != null ? Integer.parseInt(maxCd.substring(1)) + 1 : 1;
+
+        // 4자리 숫자 부분을 형식에 맞게 생성
+        String sequenceNumberString = String.format("%04d", sequenceNumber);
+
+        return prefix + sequenceNumberString;
+    }
+
     public void updateItem(ItemDto itemDto) {
+
         // dto -> entity
         ItemEntity editItemEntity = itemDto.toEntity();
+
         // itemCd에 해당하는 entity 검색
         ItemEntity itemEntity = itemRepository.findById(editItemEntity.getItemCd())
                 .orElseThrow(() -> new IllegalArgumentException("Invalid account Id:" + editItemEntity.getItemCd()));
+
         // 각 필드 업데이트
         itemEntity.setItemCd(editItemEntity.getItemCd());
         itemEntity.setItemNm(editItemEntity.getItemNm());
         itemEntity.setItemStnd(editItemEntity.getItemStnd());
         itemEntity.setItemUp(editItemEntity.getItemUp());
+
         // 엔티티 반환 후 저장
         itemRepository.save(itemEntity);
     }
 
     @Transactional
-    public void deleteItem(String itemCd) {
+    public ResponseEntity<String> deleteItem(List<String> itemCd) {
 
-        itemRepository.findById(itemCd).ifPresent(itemRepository::delete);
+        try {
+            for (String itemcd : itemCd) {
+                itemRepository.findById(itemcd).ifPresent(itemRepository::delete);
+            }
+        } catch (DataAccessException e) {
+
+            log.error("데이터베이스 조작 중 오류 발생", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("데이터베이스 조작 중 오류 발생");
+        }
+        return null;
     }
 }
