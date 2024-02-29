7b69062e01910c84f526511fb8905a18347fb3cc
LeeSang90 <dlgy900@naver.com>
2024-02-29 15:25:40 +0900

    240229 1525 거래처별 합계 계산 함수 수정

--- sixbeam/src/main/java/com/erpproject/sixbeam/ac/controller/ReceivablesController.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/ac/controller/ReceivablesController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -24,7 +25,7 @@ public class ReceivablesController {
     public String searchReceivables(Model model) {
         List<AccountEntity> actEntity = this.accountService.getList();
         model.addAttribute("actList", actEntity);
-        Map<String, Object> suminput = receivablesService.sum_input_account();
+        Map<String, Integer> suminput = receivablesService.sum_input_account(LocalDate.of(2024,2,1),LocalDate.of(2024,2,29));
 //        suminput.get(code);
 
 
