917d343bb05991a9f0e0b3e3a8b8c4c9f73024b4
jacob0701 <hello.git.rpnia@naver.com>
2024-02-29 17:54:20 +0900

    2/29 거래처별 채권 1차 완성

--- sixbeam/src/main/java/com/erpproject/sixbeam/ac/controller/ReceivablesController.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/ac/controller/ReceivablesController.java
@@ -5,11 +5,13 @@ import com.erpproject.sixbeam.ac.service.AccountService;
 import com.erpproject.sixbeam.ac.service.ReceivablesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,21 +28,30 @@ public class ReceivablesController {
     public String searchReceivables(Model model) {
         List<AccountEntity> actEntity = this.accountService.getList();
         model.addAttribute("actList", actEntity);
-        Map<String, Integer> suminput = receivablesService.sum_input_account(LocalDate.of(2024,2,1),LocalDate.of(2024,2,29));
-//        suminput.get(code);
-
         return "contents/ac/receivables";
     }
-    @PostMapping("/receivables/receivables")
-    public String inputAct(
+    @GetMapping("/receivables/load")
+    public ResponseEntity<?> inputAct(Model model,
             @RequestParam("accountCode") String accountCode,
-            @RequestParam("accountNm") String accountNm,
+            @RequestParam("accountName") String accountName,
             @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate)
     {
+        Map<String, Integer> suminput = receivablesService.sum_input_account(startDate,endDate);
+        int receivablesSum = suminput.get(accountCode);
+        int basicReceivables = 0;
+        model.addAttribute("거래처명 코드", accountCode);
+        model.addAttribute("거래처명", accountName);
+        model.addAttribute("기초채권", basicReceivables);
+        model.addAttribute("재고매출", receivablesSum);
 
+        Map<String,Object> map =new HashMap<>();
+        map.put("accountCode", accountCode);
+        map.put("accountName", accountName);
+        map.put("basicReceivables", basicReceivables);
+        map.put("receivablesSum", receivablesSum);
 
-        return "contents/ac/receivables";
+        return ResponseEntity.ok().body(map);
     }
 
 }
