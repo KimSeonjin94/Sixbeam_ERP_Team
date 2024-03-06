fc63172b17a9e4807fb8734612f3b5b5a9d22608
pjh4554 <dnflgkrry28@gmail.com>
2024-03-06 17:53:54 +0900

    03-06 17:51 다시 완벽하게 고쳤어요

--- sixbeam/src/main/java/com/erpproject/sixbeam/main/SecurityConfig.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/main/SecurityConfig.java
@@ -17,7 +17,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
-@Configuration
+@Configuration//ㅋㅋ
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
