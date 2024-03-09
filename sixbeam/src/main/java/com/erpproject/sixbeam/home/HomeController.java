a3d91b1a1dfa8f6c81218ea3b6826ea3059de2c9
pjh4554 <dnflgkrry28@gmail.com>
2024-03-10 01:08:17 +0900

    03-10 풀캘린더 진짜 완료 + 프로필 버튼 완료

--- sixbeam/src/main/java/com/erpproject/sixbeam/home/HomeController.java
+++ sixbeam/src/main/java/com/erpproject/sixbeam/home/HomeController.java
@@ -1,17 +1,24 @@
 package com.erpproject.sixbeam.home;
 
+import com.erpproject.sixbeam.hr.entity.EmpInfoEntity;
+import com.erpproject.sixbeam.hr.repository.EmpInfoRepository;
+import com.erpproject.sixbeam.hr.service.EmpInfoService;
 import com.erpproject.sixbeam.pd.service.OrderService;
 import com.erpproject.sixbeam.pur.entity.OrinPutEntity;
 import com.erpproject.sixbeam.pur.service.OrinPutService;
 import com.erpproject.sixbeam.ss.entity.EstimateEntity;
 import com.erpproject.sixbeam.ss.service.EstimateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/sixbeam")
 @RequiredArgsConstructor
@@ -20,6 +27,8 @@ public class HomeController {
     private final EstimateService estimateService;
     private final OrinPutService orinPutService;
 //    private final OrderService orderService;
+    private final EmpInfoService empInfoService;
+    private final EmpInfoRepository empInfoRepository;
 
     @GetMapping("/home")
     public String sixbeam_main(Model model) {
@@ -29,5 +38,14 @@ public class HomeController {
         model.addAttribute("estimateEntities",estimateEntities);
         model.addAttribute("orinPutEntities",orinPutEntities);
         return "contents/home/Home_Form";
+    }
+    @GetMapping("/profile")
+    public String profile(Model model){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Long empInfoId = Long.parseLong(username);
+        Optional<EmpInfoEntity> empInfoEntityOptional = empInfoRepository.findByEmpInfoId(empInfoId);
+        // Optional을 직접 사용
+        model.addAttribute("empInfoOne", empInfoEntityOptional.orElseThrow(() -> new RuntimeException("사용자 정보를 찾을 수 없습니다.")));
+        return "contents/home/Profile_Form";
     }
 }
