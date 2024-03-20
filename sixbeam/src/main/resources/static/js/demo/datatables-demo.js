94e878b3ff4a27cf72c338b7c727888fa79cf491
pjh4554 <dnflgkrry28@gmail.com>
2024-03-20 11:59:49 +0900

    03-20 12:00

--- sixbeam/src/main/resources/static/js/demo/datatables-demo.js
+++ sixbeam/src/main/resources/static/js/demo/datatables-demo.js
@@ -177,6 +177,301 @@ $(document).ready(function () {
 
     });
 
+    //zzzzz
+    $('#dataTableSalaryId').DataTable({
+        "language": {
+            "emptyTable": "데이터가 없어요.",
+            "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+            "info": "현재 _START_ - _END_ / _TOTAL_건",
+            "infoEmpty": "데이터 없음",
+            "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+            "search": "검색: ",
+            "zeroRecords": "일치하는 데이터가 없어요.",
+            "loadingRecords": "로딩중...",
+            "processing": "잠시만 기다려 주세요...",
+            "paginate": {
+                "next": "다음",
+                "previous": "이전"
+            }
+        },
+        "buttons": [
+            //            {
+            //                text: '신규',
+            //                className: 'btn btn-primary mr-sm-1',
+            //                action: function ( e, dt, node, config ) {
+            //                    $('#createPurModal').modal('show');
+            //                }
+            //            },
+            //            {
+            //                text: '삭제',
+            //                className: 'btn btn-danger mr-auto',
+            //                action: function ( e, dt, node, config ) {
+            //                    $('#deletePurModal').modal('show');
+            //                }
+            //            },
+            {
+                extend: 'copy',
+                className: 'btn btn-left mr-sm-1'
+            },
+            {
+                extend: 'excel',
+                className: 'btn btn-left mr-sm-1'
+            },
+            {
+                extend: 'print',
+                className: 'btn btn-left ml-sm-1'
+            }],
+        "order": orderOption
+        ,
+        "initComplete": function () {
+            // DataTables 초기화가 완료된 후 클래스 추가
+            var buttonGroup = '<div class = "btn-group" role = "group" aria-label = "Basic example">' +
+            '<button type = "button" class = "btn btn-primary" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
+            '<button type="button" class="btn btn-warning" data-toggle="modal" onclick="updateSelectedSalaryId()">수정</button>' +
+            '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteSalaryIdModal">삭제</button>' +
+            '</div>';
+
+            // 버튼 그룹을 DataTables의 버튼 컨테이너 앞에 추가
+            this.api().buttons().container().prepend(buttonGroup);
+        }
+
+    });
+    $('#dataTableReason').DataTable({
+        "language": {
+            "emptyTable": "데이터가 없어요.",
+            "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+            "info": "현재 _START_ - _END_ / _TOTAL_건",
+            "infoEmpty": "데이터 없음",
+            "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+            "search": "검색: ",
+            "zeroRecords": "일치하는 데이터가 없어요.",
+            "loadingRecords": "로딩중...",
+            "processing": "잠시만 기다려 주세요...",
+            "paginate": {
+                "next": "다음",
+                "previous": "이전"
+            }
+        },
+        "buttons": [
+            //            {
+            //                text: '신규',
+            //                className: 'btn btn-primary mr-sm-1',
+            //                action: function ( e, dt, node, config ) {
+            //                    $('#createPurModal').modal('show');
+            //                }
+            //            },
+            //            {
+            //                text: '삭제',
+            //                className: 'btn btn-danger mr-auto',
+            //                action: function ( e, dt, node, config ) {
+            //                    $('#deletePurModal').modal('show');
+            //                }
+            //            },
+            {
+                extend: 'copy',
+                className: 'btn btn-left mr-sm-1'
+            },
+            {
+                extend: 'excel',
+                className: 'btn btn-left mr-sm-1'
+            },
+            {
+                extend: 'print',
+                className: 'btn btn-left ml-sm-1'
+            }],
+        "order": orderOption
+        ,
+        "initComplete": function () {
+            // DataTables 초기화가 완료된 후 클래스 추가
+            var buttonGroup = '<div class = "btn-group" role = "group" aria-label = "Basic example">' +
+            '<button type = "button" class = "btn btn-primary" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
+            '<button type="button" class="btn btn-warning" data-toggle="modal" onclick="updateSelectedReason()">수정</button>' +
+            '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteReasonModal">삭제</button>' +
+            '</div>';
+
+            // 버튼 그룹을 DataTables의 버튼 컨테이너 앞에 추가
+            this.api().buttons().container().prepend(buttonGroup);
+        }
+
+    });
+    $('#dataTablePosition').DataTable({
+        "language": {
+            "emptyTable": "데이터가 없어요.",
+            "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+            "info": "현재 _START_ - _END_ / _TOTAL_건",
+            "infoEmpty": "데이터 없음",
+            "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+            "search": "검색: ",
+            "zeroRecords": "일치하는 데이터가 없어요.",
+            "loadingRecords": "로딩중...",
+            "processing": "잠시만 기다려 주세요...",
+            "paginate": {
+                "next": "다음",
+                "previous": "이전"
+            }
+        },
+        "buttons": [
+            //            {
+            //                text: '신규',
+            //                className: 'btn btn-primary mr-sm-1',
+            //                action: function ( e, dt, node, config ) {
+            //                    $('#createPurModal').modal('show');
+            //                }
+            //            },
+            //            {
+            //                text: '삭제',
+            //                className: 'btn btn-danger mr-auto',
+            //                action: function ( e, dt, node, config ) {
+            //                    $('#deletePurModal').modal('show');
+            //                }
+            //            },
+            {
+                extend: 'copy',
+                className: 'btn btn-left mr-sm-1'
+            },
+            {
+                extend: 'excel',
+                className: 'btn btn-left mr-sm-1'
+            },
+            {
+                extend: 'print',
+                className: 'btn btn-left ml-sm-1'
+            }],
+        "order": orderOption
+        ,
+        "initComplete": function () {
+            // DataTables 초기화가 완료된 후 클래스 추가
+            var buttonGroup = '<div class = "btn-group" role = "group" aria-label = "Basic example">' +
+            '<button type = "button" class = "btn btn-primary" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
+            '<button type="button" class="btn btn-warning" data-toggle="modal" onclick="updateSelectedPosition()">수정</button>' +
+            '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deletePositionModal">삭제</button>' +
+            '</div>';
+
+            // 버튼 그룹을 DataTables의 버튼 컨테이너 앞에 추가
+            this.api().buttons().container().prepend(buttonGroup);
+        }
+
+    });
+
+
+    $('#dataTableDepart').DataTable({
+        "language": {
+            "emptyTable": "데이터가 없어요.",
+            "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+            "info": "현재 _START_ - _END_ / _TOTAL_건",
+            "infoEmpty": "데이터 없음",
+            "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+            "search": "검색: ",
+            "zeroRecords": "일치하는 데이터가 없어요.",
+            "loadingRecords": "로딩중...",
+            "processing": "잠시만 기다려 주세요...",
+            "paginate": {
+                "next": "다음",
+                "previous": "이전"
+            }
+        },
+        "buttons": [
+            //            {
+            //                text: '신규',
+            //                className: 'btn btn-primary mr-sm-1',
+            //                action: function ( e, dt, node, config ) {
+            //                    $('#createPurModal').modal('show');
+            //                }
+            //            },
+            //            {
+            //                text: '삭제',
+            //                className: 'btn btn-danger mr-auto',
+            //                action: function ( e, dt, node, config ) {
+            //                    $('#deletePurModal').modal('show');
+            //                }
+            //            },
+            {
+                extend: 'copy',
+                className: 'btn btn-left mr-sm-1'
+            },
+            {
+                extend: 'excel',
+                className: 'btn btn-left mr-sm-1'
+            },
+            {
+                extend: 'print',
+                className: 'btn btn-left ml-sm-1'
+            }],
+        "order": orderOption
+        ,
+        "initComplete": function () {
+            // DataTables 초기화가 완료된 후 클래스 추가
+            var buttonGroup = '<div class = "btn-group" role = "group" aria-label = "Basic example">' +
+            '<button type = "button" class = "btn btn-primary" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
+            '<button type="button" class="btn btn-warning" data-toggle="modal" onclick="updateSelectedDepart()">수정</button>' +
+            '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteDepartModal">삭제</button>' +
+            '</div>';
+
+            // 버튼 그룹을 DataTables의 버튼 컨테이너 앞에 추가
+            this.api().buttons().container().prepend(buttonGroup);
+        }
+
+    });
+
+    $('#dataTableSalary').DataTable({
+        "language": {
+            "emptyTable": "데이터가 없어요.",
+            "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+            "info": "현재 _START_ - _END_ / _TOTAL_건",
+            "infoEmpty": "데이터 없음",
+            "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+            "search": "검색: ",
+            "zeroRecords": "일치하는 데이터가 없어요.",
+            "loadingRecords": "로딩중...",
+            "processing": "잠시만 기다려 주세요...",
+            "paginate": {
+                "next": "다음",
+                "previous": "이전"
+            }
+        },
+        "buttons": [
+            //            {
+            //                text: '신규',
+            //                className: 'btn btn-primary mr-sm-1',
+            //                action: function ( e, dt, node, config ) {
+            //                    $('#createPurModal').modal('show');
+            //                }
+            //            },
+            //            {
+            //                text: '삭제',
+            //                className: 'btn btn-danger mr-auto',
+            //                action: function ( e, dt, node, config ) {
+            //                    $('#deletePurModal').modal('show');
+            //                }
+            //            },
+            {
+                extend: 'copy',
+                className: 'btn btn-left mr-sm-1'
+            },
+            {
+                extend: 'excel',
+                className: 'btn btn-left mr-sm-1'
+            },
+            {
+                extend: 'print',
+                className: 'btn btn-left ml-sm-1'
+            }],
+        "order": orderOption
+        ,
+        "initComplete": function () {
+            // DataTables 초기화가 완료된 후 클래스 추가
+            var buttonGroup = '<div class="btn-group" role="group" aria-label="Basic example">' +
+            '<button type = "button" class = "btn btn-primary" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
+            '<button type="button" class="btn btn-warning" data-toggle="modal" onclick="updateSelectedSalary()">수정</button>' +
+            '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteSalaryModal">삭제</button>' +
+            '</div>';
+
+            // 버튼 그룹을 DataTables의 버튼 컨테이너 앞에 추가
+            this.api().buttons().container().prepend(buttonGroup);
+        }
+
+    });
+
     $('#dataTableHr').DataTable({
         "language": {
             "emptyTable": "데이터가 없어요.",
