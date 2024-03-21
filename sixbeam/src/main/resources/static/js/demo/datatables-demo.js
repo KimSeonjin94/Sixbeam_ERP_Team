2304c248483719c76dea7767f74159843a8b2fcd
LIMGEES <149366415+LIMGEES@users.noreply.github.com>
2024-03-21 14:57:20 +0900

    03.21 출석 체크 해결

--- sixbeam/src/main/resources/static/js/demo/datatables-demo.js
+++ sixbeam/src/main/resources/static/js/demo/datatables-demo.js
@@ -117,13 +117,13 @@ $(document).ready(function () {
                     $('#newAccount').modal('show');
                 }
             },
-//            {
-//                text: '수정',
-//                className: 'btn btn-warning mr-sm-1',
-//                action: function (e, dt, node, config) {
-//                    $('#editAccount').modal('show');
-//                }
-//            },
+            //            {
+            //                text: '수정',
+            //                className: 'btn btn-warning mr-sm-1',
+            //                action: function (e, dt, node, config) {
+            //                    $('#editAccount').modal('show');
+            //                }
+            //            },
             {
                 text: '삭제',
                 className: 'btn btn-danger mr-auto',
@@ -225,14 +225,13 @@ $(document).ready(function () {
         ,
         "initComplete": function () {
             // DataTables 초기화가 완료된 후 클래스 추가
-            var buttonGroup = '<div class = "btn-group" role = "group" aria-label = "Basic example">' +
-            '<button type = "button" class = "btn btn-primary" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
-            '<button type="button" class="btn btn-warning" data-toggle="modal" onclick="updateSelectedSalaryId()">수정</button>' +
-            '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteSalaryIdModal">삭제</button>' +
-            '</div>';
+            var buttonGroup = '<button type = "button" class = "btn btn-primary mr-sm-1" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
+            '<button type="button" class="btn btn-warning mr-sm-1" data-toggle="modal" onclick="updateSelectedSalaryId()">수정</button>' +
+            '<button type="button" class="btn btn-danger mr-auto" data-toggle="modal" data-target="#deleteSalaryIdModal">삭제</button>' ;
 
             // 버튼 그룹을 DataTables의 버튼 컨테이너 앞에 추가
             this.api().buttons().container().prepend(buttonGroup);
+            this.api().buttons().container().addClass("col-sm-12 d-flex justify-content-between");
         }
 
     });
@@ -283,14 +282,13 @@ $(document).ready(function () {
         ,
         "initComplete": function () {
             // DataTables 초기화가 완료된 후 클래스 추가
-            var buttonGroup = '<div class = "btn-group" role = "group" aria-label = "Basic example">' +
-            '<button type = "button" class = "btn btn-primary" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
-            '<button type="button" class="btn btn-warning" data-toggle="modal" onclick="updateSelectedReason()">수정</button>' +
-            '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteReasonModal">삭제</button>' +
-            '</div>';
+            var buttonGroup = '<button type = "button" class = "btn btn-primary mr-sm-1" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
+            '<button type="button" class="btn btn-warning mr-sm-1" data-toggle="modal" onclick="updateSelectedReason()">수정</button>' +
+            '<button type="button" class="btn btn-danger mr-auto" data-toggle="modal" data-target="#deleteReasonModal">삭제</button>';
 
             // 버튼 그룹을 DataTables의 버튼 컨테이너 앞에 추가
             this.api().buttons().container().prepend(buttonGroup);
+            this.api().buttons().container().addClass("col-sm-12 d-flex justify-content-between");
         }
 
     });
@@ -341,14 +339,13 @@ $(document).ready(function () {
         ,
         "initComplete": function () {
             // DataTables 초기화가 완료된 후 클래스 추가
-            var buttonGroup = '<div class = "btn-group" role = "group" aria-label = "Basic example">' +
-            '<button type = "button" class = "btn btn-primary" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
-            '<button type="button" class="btn btn-warning" data-toggle="modal" onclick="updateSelectedPosition()">수정</button>' +
-            '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deletePositionModal">삭제</button>' +
-            '</div>';
+            var buttonGroup = '<button type = "button" class = "btn btn-primary mr-sm-1" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
+            '<button type="button" class="btn btn-warning mr-sm-1" data-toggle="modal" onclick="updateSelectedPosition()">수정</button>' +
+            '<button type="button" class="btn btn-danger mr-auto" data-toggle="modal" data-target="#deletePositionModal">삭제</button>';
 
             // 버튼 그룹을 DataTables의 버튼 컨테이너 앞에 추가
             this.api().buttons().container().prepend(buttonGroup);
+            this.api().buttons().container().addClass("col-sm-12 d-flex justify-content-between");
         }
 
     });
@@ -401,14 +398,13 @@ $(document).ready(function () {
         ,
         "initComplete": function () {
             // DataTables 초기화가 완료된 후 클래스 추가
-            var buttonGroup = '<div class = "btn-group" role = "group" aria-label = "Basic example">' +
-            '<button type = "button" class = "btn btn-primary" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
-            '<button type="button" class="btn btn-warning" data-toggle="modal" onclick="updateSelectedDepart()">수정</button>' +
-            '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteDepartModal">삭제</button>' +
-            '</div>';
+            var buttonGroup = '<button type = "button" class = "btn btn-primary mr-sm-1" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
+            '<button type="button" class="btn btn-warning mr-sm-1" data-toggle="modal" onclick="updateSelectedDepart()">수정</button>' +
+            '<button type="button" class="btn btn-danger mr-auto" data-toggle="modal" data-target="#deleteDepartModal">삭제</button>';
 
             // 버튼 그룹을 DataTables의 버튼 컨테이너 앞에 추가
             this.api().buttons().container().prepend(buttonGroup);
+            this.api().buttons().container().addClass("col-sm-12 d-flex justify-content-between");
         }
 
     });
@@ -460,14 +456,13 @@ $(document).ready(function () {
         ,
         "initComplete": function () {
             // DataTables 초기화가 완료된 후 클래스 추가
-            var buttonGroup = '<div class="btn-group" role="group" aria-label="Basic example">' +
-            '<button type = "button" class = "btn btn-primary" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
-            '<button type="button" class="btn btn-warning" data-toggle="modal" onclick="updateSelectedSalary()">수정</button>' +
-            '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteSalaryModal">삭제</button>' +
-            '</div>';
+            var buttonGroup = '<button type = "button" class = "btn btn-primary mr-sm-1" data-toggle = "modal" data-target = "#newestimate">등록</button>' +
+            '<button type="button" class="btn btn-warning mr-sm-1" data-toggle="modal" onclick="updateSelectedSalary()">수정</button>' +
+            '<button type="button" class="btn btn-danger mr-auto" data-toggle="modal" data-target="#deleteSalaryModal">삭제</button>';
 
             // 버튼 그룹을 DataTables의 버튼 컨테이너 앞에 추가
             this.api().buttons().container().prepend(buttonGroup);
+            this.api().buttons().container().addClass("col-sm-12 d-flex justify-content-between");
         }
 
     });
@@ -489,20 +484,20 @@ $(document).ready(function () {
             }
         },
         "buttons": [
-//            {
-//                text: '신규',
-//                className: 'btn btn-primary mr-sm-1',
-//                action: function ( e, dt, node, config ) {
-//                    $('#createPurModal').modal('show');
-//                }
-//            },
-//            {
-//                text: '삭제',
-//                className: 'btn btn-danger mr-auto',
-//                action: function ( e, dt, node, config ) {
-//                    $('#deletePurModal').modal('show');
-//                }
-//            },
+            //            {
+            //                text: '신규',
+            //                className: 'btn btn-primary mr-sm-1',
+            //                action: function ( e, dt, node, config ) {
+            //                    $('#createPurModal').modal('show');
+            //                }
+            //            },
+            //            {
+            //                text: '삭제',
+            //                className: 'btn btn-danger mr-auto',
+            //                action: function ( e, dt, node, config ) {
+            //                    $('#deletePurModal').modal('show');
+            //                }
+            //            },
             {
                 extend: 'copy',
                 className: 'btn btn-left mr-sm-1'
@@ -519,14 +514,13 @@ $(document).ready(function () {
         ,
         "initComplete": function () {
             // DataTables 초기화가 완료된 후 클래스 추가
-            var buttonGroup = '<div class="btn-group" role="group" aria-label="Basic example">' +
-                '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#newestimate">등록</button>' +
-                '<button type="button" class="btn btn-warning" onclick="updateSelectedEmpInfo()">수정</button>' +
-                '<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteEmployeeModal">삭제</button>' +
-                '</div>';
+            var buttonGroup = '<button type="button" class="btn btn-primary mr-sm-1" data-toggle="modal" data-target="#newestimate">등록</button>' +
+            '<button type="button" class="btn btn-warning mr-sm-1" onclick="updateSelectedEmpInfo()">수정</button>' +
+            '<button type="button" class="btn btn-danger mr-auto" data-toggle="modal" data-target="#deleteEmployeeModal">삭제</button>';
 
             // 버튼 그룹을 DataTables의 버튼 컨테이너 앞에 추가
             this.api().buttons().container().prepend(buttonGroup);
+            this.api().buttons().container().addClass("col-sm-12 d-flex justify-content-between");
         }
 
     });
@@ -641,43 +635,43 @@ $(document).ready(function () {
         }
     });
     $('#dataTableStCh').DataTable({
-            "language": {
-                "emptyTable": "데이터가 없어요.",
-                "lengthMenu": "페이지당 _MENU_ 개씩 보기",
-                "info": "현재 _START_ - _END_ / _TOTAL_건",
-                "infoEmpty": "데이터 없음",
-                "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
-                "search": "검색: ",
-                "zeroRecords": "일치하는 데이터가 없어요.",
-                "loadingRecords": "로딩중...",
-                "processing": "잠시만 기다려 주세요...",
-                "paginate": {
-                    "next": "다음",
-                    "previous": "이전"
-                }
-            },
-            buttons: [
-                {
-                    extend: 'copy',
-                    className: 'btn btn-right ml-sm-1'
-                },
-                {
-                    extend: 'excel',
-                    className: 'btn btn-right ml-sm-1'
-                },
-                {
-                    extend: 'print',
-                    className: 'btn btn-right ml-sm-1'
-                }
-            ],
-
-            "order": [[1, "desc"]],
-            initComplete: function () {
-                // DataTables 초기화가 완료된 후 클래스 추가
-                this.api().buttons().container().addClass("col-sm-12 d-flex justify-content-end");
-
+        "language": {
+            "emptyTable": "데이터가 없어요.",
+            "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+            "info": "현재 _START_ - _END_ / _TOTAL_건",
+            "infoEmpty": "데이터 없음",
+            "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+            "search": "검색: ",
+            "zeroRecords": "일치하는 데이터가 없어요.",
+            "loadingRecords": "로딩중...",
+            "processing": "잠시만 기다려 주세요...",
+            "paginate": {
+                "next": "다음",
+                "previous": "이전"
             }
-        });
+        },
+        buttons: [
+            {
+                extend: 'copy',
+                className: 'btn btn-right ml-sm-1'
+            },
+            {
+                extend: 'excel',
+                className: 'btn btn-right ml-sm-1'
+            },
+            {
+                extend: 'print',
+                className: 'btn btn-right ml-sm-1'
+            }
+        ],
+
+        "order": [[1, "desc"]],
+        initComplete: function () {
+            // DataTables 초기화가 완료된 후 클래스 추가
+            this.api().buttons().container().addClass("col-sm-12 d-flex justify-content-end");
+
+        }
+    });
     $('#dataTablePd').DataTable({
         "language": {
             "emptyTable": "데이터가 없어요.",
